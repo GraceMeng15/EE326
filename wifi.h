d00a2c8d3cd76f27f417c8775722d914a12578f9
Grace Meng <gracemeng@dhcp-10-105-221-169.wireless.northwestern.private>
2025-02-10 14:59:08 -0600

    modified wifi.h

--- wifi.h
+++ wifi.h
@@ -9,7 +9,20 @@
 #ifndef WIFI_H_
 #define WIFI_H_
 
-
+void wifi usart handler(void);
+void process incoming byte wifi(uint8 t in byte);
+void wifi command response handler(uint32 t ul id, uint32 t ul mask);
+void process data wifi (void);
+void wifi provision handler(uint32 t ul id, uint32 t ul mask);
+void wifi spi handler(void);
+void configure usart wifi(void);
+void configure wifi comm pin(void);
+void configure wifi provision pin(void);
+void configure spi(void);
+void spi peripheral initialize(void);
+void prepare spi transfer(void);
+void write wifi command(char* comm, uint8 t cnt);
+void write image to web(void);
 
 
 
