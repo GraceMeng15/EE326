5357d618709a7795989fe6bdca9c61caff0b5422
Grace Meng <gracemeng@dhcp-10-105-221-169.wireless.northwestern.private>
2025-02-24 14:54:16 -0600

    added to wifi.h

--- firmware/src/wifi.h
+++ firmware/src/wifi.h
@@ -9,7 +9,20 @@
 #ifndef WIFI_H_
 #define WIFI_H_
 
-
+void wifi_usart_handler(void);
+void process_incoming_byte_wifi(uint8 t in byte);
+void wifi_command_response_handler(uint32 t ul id, uint32 t ul mask);
+void process_data_wifi (void);
+void wifi_provision_handler(uint32 t ul id, uint32 t ul mask);
+void wifi spi handler(void);
+void configure usart wifi(void);
+void configure wifi comm pin(void);
+void configure wifi provision pin(void);
+void configure spi(void);
+void spi peripheral initialize(void);
+void prepare spi transfer(void);
+void write wifi command(char* comm, uint8 t cnt);
+void write image to web(void);
 
 
 
